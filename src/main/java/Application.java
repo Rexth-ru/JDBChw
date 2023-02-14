efe4d3ea2c769d2276aba60f2925c7e42426f569
Rexth-ru <119601642+Rexth-ru@users.noreply.github.com>
2023-02-14 16:17:47 +0300

    implementations via Hibernate

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -18,30 +18,5 @@ public class Application {
 //        List<Employee> list = employeeDAO.getAllEmployees();
 //        list.stream().forEach(System.out::println);
         employeeDAO.delete(new Employee(9,"Danny","Kim","man",17,5));
-
     }
 }
-
-
-
-
-
-//        final String user = "postgres";
-//        final String password = "469841";
-//        final String url = "jdbc:postgresql://localhost:5432/skypro";
-//        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-//            EmployeeDAO employeeDAO = new EmployeeDAOImpl(connection);
-//            List<Employee> employeeList = employeeDAO.getAllEmployees();
-//            for (Employee em : employeeList) {
-//                System.out.println(em);
-//            }
-//            employeeDAO.deleteById(2);
-//            employeeDAO.updateById(5, 25);
-//
-//            Employee employee = new Employee(14, "Tom", "Kolins", "man", 44, new City(4));
-//            employeeDAO.create(employee);
-//            System.out.println(employeeDAO.getById(7));
-//        }
-//    }
-//}
-//
